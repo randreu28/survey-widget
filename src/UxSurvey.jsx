5aee456a15901ca26ea93e6d7679fd4abef6304f
Rubén Chiquin <randreu28@gmail.com>
2022-02-24 10:52:52 +0100

    Formatting and functions optimization

--- src/UxSurvey.jsx
+++ src/UxSurvey.jsx
@@ -1,22 +1,20 @@
-import { useState, useEffect } from 'react'
-import { Transition } from '@headlessui/react'
+import { useState, useEffect } from "react";
+import { Transition } from "@headlessui/react";
 
-import Consent from './components/Consent.jsx'
-import Ux from './components/Ux.jsx'
-import Thanks from './components/Thanks.jsx'
-
+import Consent from "./components/Consent.jsx";
+import Ux from "./components/Ux.jsx";
+import Thanks from "./components/Thanks.jsx";
 
 export default function UxSurvey() {
-
-  const [isShowing, setIsShowing] = useState(-1)
-  const [isAnimationDone, setAnimationDone] = useState(0)
+  const [isShowing, setIsShowing] = useState(-1);
+  const [isAnimationDone, setAnimationDone] = useState(0);
 
   /* Espera 25000ms, luego monta el Popup. */
   useEffect(() => {
     setTimeout(function () {
-      setIsShowing(0)
-    }, 2500)
-  }, [setIsShowing])
+      setIsShowing(0);
+    }, 2500);
+  }, [setIsShowing]);
 
   /* Por implementar:
 
@@ -33,55 +31,57 @@ export default function UxSurvey() {
       <Transition
         show={isShowing !== -1}
         appear={true}
-        enter='transition-opacity duration-200'
-        enterFrom='opacity-0'
-        enterTo='opacity-100'
-        leave='transition-opacity duration-200'
-        leaveFrom='opacity-100'
-        leaveTo='opacity-0'
-        >
-        
-        <div 
-        className='fixed z-[999] p-5 bottom-0 right-0 m-3 sm:m-6 text-black bg-white rounded-xl space-y-3'>
-
+        enter="transition-opacity duration-200"
+        enterFrom="opacity-0"
+        enterTo="opacity-100"
+        leave="transition-opacity duration-200"
+        leaveFrom="opacity-100"
+        leaveTo="opacity-0"
+      >
+        <div className="fixed z-[999] p-5 bottom-0 right-0 m-3 sm:m-6 text-black bg-white rounded-xl space-y-3">
           <Transition
             show={isShowing === 0}
-            enter='transition-opacity duration-200'
-            enterFrom='opacity-0'
-            enterTo='opacity-100'
-            leave='transition-opacity duration-200'
-            leaveFrom='opacity-100'
-            leaveTo='opacity-0'
-            afterLeave={()=>{setAnimationDone(1)}}
-            >
-            <Consent setIsShowing={setIsShowing} /> 
+            enter="transition-opacity duration-200"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="transition-opacity duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+            afterLeave={() => {
+              setAnimationDone(1);
+            }}
+          >
+            <Consent setIsShowing={setIsShowing} />
           </Transition>
 
           <Transition
             show={isShowing === 1 && isAnimationDone === 1}
-            enter='transition-opacity duration-200'
-            enterFrom='opacity-0'
-            enterTo='opacity-100'
-            leave='transition-opacity duration-200'
-            leaveFrom='opacity-100'
-            leaveTo='opacity-0'
-            afterLeave={()=>{setAnimationDone(2)}}
-            >
-            <Ux setIsShowing={setIsShowing} /> 
+            enter="transition-opacity duration-200"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="transition-opacity duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+            afterLeave={() => {
+              setAnimationDone(2);
+            }}
+          >
+            <Ux setIsShowing={setIsShowing} />
           </Transition>
 
           <Transition
             show={isShowing === 2 && isAnimationDone === 2}
-            enter='transition-opacity duration-200'
-            enterFrom='opacity-0'
-            enterTo='opacity-100'
-            leave='transition-opacity duration-300'
-            leaveFrom='opacity-100'
-            leaveTo='opacity-0'>
+            enter="transition-opacity duration-200"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="transition-opacity duration-300"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
             <Thanks setIsShowing={setIsShowing} />
           </Transition>
         </div>
       </Transition>
     </>
-  )
+  );
 }
--- src/components/Ux.jsx
+++ src/components/Ux.jsx
@@ -1,77 +1,75 @@
-import Happy from '../media/Happy'
-import Neutral from '../media/Neutral'
-import Sad from '../media/Sad'
-import Unhappy from '../media/Unhappy'
-import VeryHappy from '../media/VeryHappy'
-import Logo from '../media/Logo'
-import XIcon from '../media/XIcon'
+import Happy from "../media/Happy";
+import Neutral from "../media/Neutral";
+import Sad from "../media/Sad";
+import Unhappy from "../media/Unhappy";
+import VeryHappy from "../media/VeryHappy";
+import Logo from "../media/Logo";
+import XIcon from "../media/XIcon";
 
 export default function Ux({ setIsShowing }) {
-
-  let sad = () => {
+  let dataHandler = (e) => {
     //Colect data
+    console.log(e);
 
-    setIsShowing(2)
-  }
-
-  let unhappy = () => {
-    //Colect data
-
-    setIsShowing(2)
-  }
-  let neutral = () => {
-    //Colect data
-
-    setIsShowing(2)
-  }
-  let happy = () => {
-    //Colect data
-
-    setIsShowing(2)
-
-  }
-  let veryHappy = () => {
-    //Colect data
-
-    setIsShowing(2)
-  }
+    setIsShowing(2);
+  };
 
   return (
     <>
-      <div className='flex space-x-10'>
-        <div className='flex space-x-5'>
-          <Logo className='h-10 w-10 min-w-fit my-auto hidden sm:flex' />
-          <h1 className='sm:text-xl pb-3 my-auto max-w-xs'>¿Qué tan satisfecho estás con nuestros servicios hoy?</h1>
+      <div className="flex space-x-10">
+        <div className="flex space-x-5">
+          <Logo className="h-10 w-10 min-w-fit my-auto hidden sm:flex" />
+          <h1 className="sm:text-xl pb-3 my-auto max-w-xs">
+            ¿Qué tan satisfecho estás con nuestros servicios hoy?
+          </h1>
         </div>
-        <button className='hover:bg-gray-100 rounded p-2 h-fit' onClick={() => setIsShowing(-1)}>
-          <XIcon className='h-6 w-6'/>
+        <button
+          className="hover:bg-gray-100 rounded p-2 h-fit"
+          onClick={() => setIsShowing(-1)}
+        >
+          <XIcon className="h-6 w-6" />
         </button>
       </div>
 
-      <div className='flex space-x-4 justify-center pb-1.5'>
-        <button className='hover:bg-gray-100 p-3 rounded-xl duration-100' onClick={sad}>
-          <Sad className='h-5 w-5 sm:h-10 sm:w-10' />
+      <div className="flex space-x-4 justify-center pb-1.5">
+        <button
+          className="hover:bg-gray-100 p-3 rounded-xl duration-100"
+          onClick={() => dataHandler("Sad")}
+        >
+          <Sad className="h-5 w-5 sm:h-10 sm:w-10" />
         </button>
 
-        <button className='hover:bg-gray-100 p-3 rounded-xl duration-100' onClick={unhappy}>
-          <Unhappy className='h-5 w-5 sm:h-10 sm:w-10' />
+        <button
+          className="hover:bg-gray-100 p-3 rounded-xl duration-100"
+          onClick={() => dataHandler("Unhappy")}
+        >
+          <Unhappy className="h-5 w-5 sm:h-10 sm:w-10" />
         </button>
 
-        <button className='hover:bg-gray-100 p-3 rounded-xl duration-100' onClick={neutral}>
-          <Neutral className='h-5 w-5 sm:h-10 sm:w-10' />
+        <button
+          className="hover:bg-gray-100 p-3 rounded-xl duration-100"
+          onClick={() => dataHandler("Neutral")}
+        >
+          <Neutral className="h-5 w-5 sm:h-10 sm:w-10" />
         </button>
-        <button className='hover:bg-gray-100 p-3 rounded-xl duration-100' onClick={happy}>
-          <Happy className='h-5 w-5 sm:h-10 sm:w-10' />
+        <button
+          className="hover:bg-gray-100 p-3 rounded-xl duration-100"
+          onClick={() => dataHandler("Happy")}
+        >
+          <Happy className="h-5 w-5 sm:h-10 sm:w-10" />
         </button>
 
-        <button className='hover:bg-gray-100 p-3 rounded-xl duration-100' onClick={veryHappy}>
-          <VeryHappy className='h-5 w-5 sm:h-10 sm:w-10' />
+        <button
+          className="hover:bg-gray-100 p-3 rounded-xl duration-100"
+          onClick={() => dataHandler("VeryHappy")}
+        >
+          <VeryHappy className="h-5 w-5 sm:h-10 sm:w-10" />
         </button>
       </div>
-      <div className='flex justify-between sm:text-lg'>
+      <div className="flex justify-between sm:text-lg">
         <h2>Insatisfecho</h2>
         <h2>Satisfecho</h2>
       </div>
     </>
-  )
+  );
 }
