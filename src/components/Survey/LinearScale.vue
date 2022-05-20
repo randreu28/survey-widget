1071fbed9c2d970fa10e655531d89cfe7a031118
randreu28 <randreu28@gmail.com>
2022-05-20 17:21:17 +0200

    Extracts Linear scale type into a component
    In preparation for accepting multiple types of surveys, being the one that is already implemented (a linear scale type) as one of them

---
+++ src/components/Survey/LinearScale.vue
@@ -0,0 +1,91 @@
+<template>
+  <div>
+    <div class="flex space-x-4 justify-center pb-1.5">
+      <button
+        class="hover:bg-gray-100 p-3 rounded-xl duration-100"
+        @click="getData(1)"
+      >
+        <Sad class="h-5 w-5 sm:h-10 sm:w-10" />
+      </button>
+      <button
+        class="hover:bg-gray-100 p-3 rounded-xl duration-100"
+        @click="getData(2)"
+      >
+        <Unhappy class="h-5 w-5 sm:h-10 sm:w-10" />
+      </button>
+      <button
+        class="hover:bg-gray-100 p-3 rounded-xl duration-100"
+        @click="getData(3)"
+      >
+        <Neutral class="h-5 w-5 sm:h-10 sm:w-10" />
+      </button>
+      <button
+        class="hover:bg-gray-100 p-3 rounded-xl duration-100"
+        @click="getData(4)"
+      >
+        <Happy class="h-5 w-5 sm:h-10 sm:w-10" />
+      </button>
+      <button
+        class="hover:bg-gray-100 p-3 rounded-xl duration-100"
+        @click="getData(5)"
+      >
+        <VeryHappy class="h-5 w-5 sm:h-10 sm:w-10" />
+      </button>
+    </div>
+    <div class="flex justify-between sm:text-lg">
+      <h2>Insatisfecho</h2>
+      <h2>Satisfecho</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+import XIcon from "../../media/XIcon.vue";
+import Sad from "../../media/Sad.vue";
+import Unhappy from "../../media/Unhappy.vue";
+import Neutral from "../../media/Neutral.vue";
+import Happy from "../../media/Happy.vue";
+import VeryHappy from "../../media/VeryHappy.vue";
+import { store } from "../../store.js";
+
+export default {
+  components: {
+    XIcon,
+    Sad,
+    Unhappy,
+    Neutral,
+    Happy,
+    VeryHappy,
+  },
+  data() {
+    return {
+      store,
+    };
+  },
+  methods: {
+    getData(a) {
+      store.currentTab = "Thanks";
+      const date = new Date();
+      const data = {
+        ux: a,
+        date: date,
+        userLang: navigator.language || navigator.userLanguage,
+        url: window.location.href,
+        client_id: store.client_id,
+        //...
+      };
+      fetch("http://httpbin.org/post?key=abc123", {
+        method: "POST",
+        body: JSON.stringify(data),
+      })
+        .then((response) => response.json())
+        .then((response) => {
+          console.log(response);
+        }),
+        (error) => {
+          console.error(error);
+        };
+    },
+  },
+}
+</script>
